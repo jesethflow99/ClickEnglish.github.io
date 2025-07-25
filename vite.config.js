10587f2194317866b8cbbb5ffda81f4225db1be8
Edgar <ceggarr199@gmail.com>
2025-07-25 13:51:31 -0600

    page

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/ClickEnglish.github.io/', // 🔥 Base URL for deployment
   plugins: [react()],
   server: {
     host: '0.0.0.0', // 🔥 Escucha en todas las interfaces de red
