c5cf2483e482978443f81fcb352fba2ef1d5a0ea
Edgar Jesus Rascon Montes <66195237+jesethflow99@users.noreply.github.com>
2026-01-14 02:29:17 +0000

    renew

--- src/layout/Hero.jsx
+++ src/layout/Hero.jsx
@@ -8,8 +8,7 @@ const Hero = () => {
       <Ofert text="¿Tu hijo necesita aprender inglés para no quedarse atrás?- Click English es la solución para que tu hijo este preparado para la escuela." />
       <div className="content">
         <h1>Aprende Inglés <b>Click</b></h1>
-        <p>Nuestra plataforma interactiva te ayuda a dominar el inglés con recursos personalizados, foros de discusión y asistencia en tiempo real.</p>
-        <a className="btn btn-warning col-6" href='https://jesethflow99.github.io/chatbot.github.io/' target="_blank">Prueba nuestro chatbot </a>
+       
       </div>  
     </div>
   )
